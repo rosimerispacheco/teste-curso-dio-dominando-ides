26cc187ca4ab2ea6ea7d76a33228936a9c44ba56
Rosimeri Pacheco <rosimeri.spacheco@gmail.com>
2022-09-29 17:59:31 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+			Livro livro = new Livro();
+			Gato gato = new Gato();
+			
+			System.out.println(gato);
+			System.out.println(livro);
+	}
+	
+
+}
+class Livro{
+	public String livro;
+}
+
